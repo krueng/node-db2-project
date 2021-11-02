d4bdeb134c5741b4e2b3c2fbbb82e0479ef9cbe9
Adam K <krueng@gmail.com>
2021-11-02 18:21:16 -0500

    Initited cars-router

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,10 +1,11 @@
 const db = require('../../data/db-config')
+
 const getAll = () => {
   return db('cars')
 }
 
-const getById = () => {
-
+const getById = (id) => {
+  return db('cars').where('id', id).first()
 }
 
 const create = () => {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const Car = require('./cars-model')
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+} = require('./cars-middleware')
 
 const router = express.Router()
 
@@ -11,9 +17,16 @@ router.get('/', async (req, res, next) => {
         next(error)
     }
 })
-router.get('/:id', async (req, res, next) => {
-    res.json(`Getting cars with id ${req.params.id}`)
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    // try {
+    //     const car = await Car.getById(req.params.id)
+        res.json(req.car)
+    // } catch (error) {
+    //     next(error)
+    // }
 })
+
 router.post('/', async (req, res, next) => {
     res.json('Posting new car')
 })
