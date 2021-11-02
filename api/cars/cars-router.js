e9706807fdb5fe792ea5966e84d50038af621e36
Adam K <krueng@gmail.com>
2021-11-02 16:38:18 -0500

    Initiated cars-router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get ('/', async (req, res, next) => {
+    res.json('Getting all cars')
+})
+router.get ('/:id', async (req, res, next) => {
+    res.json(`Getting cars with id ${req.params.id}` )
+})
+router.post ('/', async (req, res, next) => {
+    res.json('Posting new car')
+})
+
+module.exports = router
