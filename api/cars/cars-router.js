27292014633f4d4f3f669306678440038ed487c2
Adam K <krueng@gmail.com>
2021-11-02 17:49:20 -0500

    Initited seed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const Car = require('./cars-model')
 
 const router = express.Router()
 
-router.get ('/', async (req, res, next) => {
-    res.json('Getting all cars')
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (error) {
+        next(error)
+    }
 })
-router.get ('/:id', async (req, res, next) => {
-    res.json(`Getting cars with id ${req.params.id}` )
+router.get('/:id', async (req, res, next) => {
+    res.json(`Getting cars with id ${req.params.id}`)
 })
-router.post ('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     res.json('Posting new car')
 })
 
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,45 @@
-// STRETCH
+const cars = [
+    {
+        vin: '86529635656235623',
+        make: 'Honda',
+        model: 'CR-V',
+        mileage: 892230,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '86529635656235624',
+        make: 'Ferrari',
+        model: 'CR-x',
+        mileage: 892230,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '86529635656235625',
+        make: 'Merc',
+        model: 'CR-y',
+        mileage: 892230,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '86529635656235626',
+        make: 'Lambo',
+        model: 'CR-z',
+        mileage: 892230,
+        transmission: 'automatic'
+    },
+    {
+        vin: '86529635656235627',
+        make: 'Bentley',
+        model: 'FakeCR-A',
+        mileage: 892230,
+        title: 'salvage'
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars') .truncate()
+    await knex('cars').insert(cars)
+}
